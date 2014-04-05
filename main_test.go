5d069491f71ee152377cd5d1a673051997552551
Martin Kobetic <mkobetic@gmail.com>
2014-04-05 19:10:28 -0400

    beef up tests

--- main_test.go
+++ main_test.go
@@ -6,15 +6,36 @@ import (
 )
 
 func TestExtractDomain(t *testing.T) {
-	d := extractDomain("http://www.google.com")
-	if d != "google.com" {
-		t.Fail()
+	for _, u := range []string{
+		"http://www.google.com",
+		"https://www.google.com",
+		"www.google.com",
+		"http://www.google.com/",
+		"http://www1.mmm.xxx.google.com/",
+		"http://www.y.google.com/",
+		"http://www.google.com/buh/hum",
+		"http://www.google.com/?p=1",
+		"http://www.google.com/#frag",
+		"http://www.google.com/buh?p=1&q=2",
+	} {
+		d := extractDomain(u)
+		if d != "google.com" {
+			t.Error(u)
+		}
 	}
 }
 
 func TestPwdhash(t *testing.T) {
-	pwd := pwdhash("google.com", "sesame")
-	if pwd != "yGg1DXEs" {
-		t.Error(pwd)
+	for in, out := range map[string]string{
+		"sesame":       "yGg1DXEs",
+		"seSAme":       "PEUaZ5RL",
+		"se5ame":       "PrvnMuy8",
+		"ses@me":       "jDYGye0+",
+		"sesameSESAME": "OmbRtntX7YGeCD",
+	} {
+		pwd := pwdhash("google.com", in)
+		if pwd != out {
+			t.Error(pwd)
+		}
 	}
 }
